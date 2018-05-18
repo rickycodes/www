b3d34d7b46f5353e48de9505a07e81066e0f2fc9
rickycodes <ricky.miller@gmail.com>
2018-05-18 16:58:17 -0400

    allow "deeplinking" to work-history

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ mod toggle_project;
 mod canvas;
 
 use toggle_project::toggle;
-use util::qs;
+use util::{qs, get_hash};
 use stdweb::traits::*;
 use stdweb::web::{
+  document,
   window,
   Date
 };
@@ -31,6 +32,19 @@ fn main() {
   qs(".date").set_text_content(&Date::new().get_full_year().to_string());
 
   toggle(&mut scrolls);
+
+  // scroll to #work-history and expand
+  let hash = get_hash();
+  if hash == "work-history" {
+    let input_el = document().query_selector("#cv-toggle").unwrap();
+    let scroll_to_el = document().query_selector(&format!("#{}", hash)).unwrap();
+    if input_el.is_some() && scroll_to_el.is_some() {
+      js!{ @(no_return)
+        @{input_el}.click();
+        @{scroll_to_el}.scrollIntoView();
+      }
+    }
+  }
 
   let toggle_project_event = move |_event: HashChangeEvent| {
     toggle(&mut scrolls)
