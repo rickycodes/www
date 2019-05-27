c3cde5e9dbeaec5bbf1d1eb45e1ddfbc9de8ab0f
rickycodes <ricky.miller@gmail.com>
2019-05-26 22:46:46 -0400

    update build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 PARTIALS='src/partials'
 PROJECTS=$PARTIALS/projects/
 
-echo "Generate & minify HTML..."
+echo 'Generate & minify HTML...'
 { cat ${PARTIALS}/sig.html & cat ${PARTIALS}/header.html \
     ${PARTIALS}/main.html \
     ${PROJECTS}* \
@@ -18,21 +18,14 @@ echo "Generate & minify HTML..."
 --minify-css; } > static/index.html
 
 # build
-echo "Building..."
+echo 'Building...'
 cargo web deploy --target=wasm32-unknown-unknown --release
 
 # minify gen and static js files
-jsFiles="target/deploy/*.js"
+jsFiles='target/deploy/*.js'
 for f in $jsFiles; do
     npx uglify-es $f \
         --compress \
         --mangle \
         --output $f
 done
-
-# build my blog https://github.com/rickycodes/blog
-hugo --source ~/projects/blog/
-
-# combine website and blog
-echo "Combine website and blog..."
-cp -r ~/projects/blog/public/ ./target/deploy/blog/
