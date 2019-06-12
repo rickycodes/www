46cd2291f4f70cf367379758835686cdc23ae38d
rickycodes <ricky.miller@gmail.com>
2019-06-12 18:24:01 -0400

    bigger

--- build.sh
+++ build.sh
@@ -2,30 +2,49 @@
 
 PARTIALS='src/partials'
 PROJECTS=$PARTIALS/projects/
+ARG=$1
 
-echo 'Generate & minify HTML...'
-{ cat ${PARTIALS}/sig.html & cat ${PARTIALS}/header.html \
-    ${PARTIALS}/main.html \
-    ${PROJECTS}* \
-    ${PARTIALS}/footer.html | npx html-minifier \
---collapse-whitespace \
---remove-comments \
---remove-optional-tags \
---remove-redundant-attributes \
---remove-script-type-attributes \
---remove-tag-whitespace \
---use-short-doctype \
---minify-css; } > static/index.html
+gen() {
+    echo 'Generate + minify HTML...'
+    { cat ${PARTIALS}/sig.html & cat ${PARTIALS}/header.html \
+        ${PARTIALS}/main.html \
+        ${PROJECTS}* \
+        ${PARTIALS}/footer.html | npx html-minifier \
+    --collapse-whitespace \
+    --remove-comments \
+    --remove-optional-tags \
+    --remove-redundant-attributes \
+    --remove-script-type-attributes \
+    --remove-tag-whitespace \
+    --use-short-doctype \
+    --minify-css; } > static/index.html
+}
 
-# build
-echo 'Building...'
-cargo web deploy --target=wasm32-unknown-unknown --release
+build() {
+    # build
+    echo 'Building...'
+    cargo web deploy --target=wasm32-unknown-unknown --release
+}
 
-# minify gen and static js files
-jsFiles='target/deploy/*.js'
-for f in $jsFiles; do
-    npx uglify-es $f \
-        --compress \
-        --mangle \
-        --output $f
-done
+min() {
+    # minify gen and static js files
+    echo 'Minify...'
+    jsFiles='target/deploy/*.js'
+    for f in $jsFiles; do
+        npx uglify-es $f \
+            --compress \
+            --mangle \
+            --output $f
+    done
+}
+
+if [ -z "$ARG" ]
+then
+    gen
+    build
+    min
+else
+    [ "$ARG" == "gen" ] && gen
+    [ "$ARG" == "min" ] && min
+    [ "$ARG" == "build" ] && build
+fi
