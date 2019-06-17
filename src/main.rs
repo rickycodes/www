7329cd038face8466ea513d51b485e4533bc7c2f
Ricky Miller <ricky.miller@gmail.com>
2019-06-17 01:35:05 -0400

    Don't need these I guess

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,29 @@ use work_history::{
   bind_work_toggle
 };
 use toggle_project::toggle;
-use util::qs;
+use util::{
+  qs,
+  nl
+};
 use stdweb::traits::*;
 use stdweb::web::{
+  CloneKind,
   window,
   Date
 };
 
-use stdweb::web::event::HashChangeEvent;
+use stdweb::web::event::{
+  HashChangeEvent,
+  DragStartEvent,
+  DragEndEvent,
+  DragEvent,
+  DragDropEvent,
+  DragOverEvent
+  // DragEnterEvent
+};
+
+use stdweb::unstable::TryInto;
+use stdweb::web::HtmlElement;
 
 fn main() {
   stdweb::initialize();
@@ -43,6 +58,68 @@ fn main() {
   let toggle_project_event = move |_event: HashChangeEvent| {
     toggle(&mut scrolls)
   };
+
+  qs(".coord").add_event_listener(|event: DragOverEvent| {
+    event.prevent_default();
+  });
+
+  qs(".coord").add_event_listener(|event: DragDropEvent| {
+    event.prevent_default();
+    console!(log, "drop!");
+    let del = || {
+      let el = qs("[data-dragging='true']");
+      el.set_attribute("style", "display: none;").unwrap();
+      el.set_attribute("data-dragging", "false").unwrap();
+    };
+
+    js! {
+      var del = @{del};
+      if (window.confirm("U sure?")) {
+        del();
+        del.drop();
+      }
+    }
+  });
+
+  for link in nl(".project.link, .cv.link") {
+    let el: HtmlElement = link.clone().try_into().unwrap();
+    let drag_event = |event: DragEvent| {
+      let cursor = qs(".cursor");
+      let x = f64::from(event.client_x());
+      let y = f64::from(event.client_y());
+      cursor.set_attribute("style", &format!(
+        "transform: translate3d({}px,{}px,0);", x, y
+      )).unwrap();
+    };
+
+    link.add_event_listener(enclose!( (el) move |_event: DragStartEvent| {
+      qs(".coord").set_text_content("🗑️");
+      qs(".coord").set_attribute("style", "font-size: 60px;").unwrap();
+      let cursor = qs(".cursor");
+      cursor.class_list().remove("zoom").unwrap();
+      let clone = el.clone_node(CloneKind::Deep).unwrap();
+      el.set_attribute("data-dragging", "true").unwrap();
+      cursor.append_child(&clone);
+      console!(log, qs(".coord"));
+    }));
+
+    link.add_event_listener(drag_event);
+
+    link.add_event_listener(|event: DragEndEvent| {
+      let x = f64::from(event.client_x());
+      let y = f64::from(event.client_y());
+
+      qs(".coord").set_text_content(&format!("_x: {}, _y: {}", x, y));
+      // el.set_attribute("style", "").unwrap();
+      let cursor = qs(".cursor");
+      let project = qs(".cursor .project");
+
+      let projects = qs("._projects");
+      projects.remove_attribute("style");
+      qs(".coord").remove_attribute("style");
+      cursor.remove_child(&project).unwrap();
+    });
+  }
 
   window().add_event_listener(toggle_project_event);
 
