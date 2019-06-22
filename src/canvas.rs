e8e8d8521e6e63ed70aa22c47eff4b16c86163ed
rickycodes <ricky.miller@gmail.com>
2019-06-22 15:39:04 -0400

    start using builder & rustfmt

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,76 +1,75 @@
-use util::{qs,get_range};
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
-use stdweb::web::{
-  document,
-  window,
-  CanvasRenderingContext2d,
-  html_element::CanvasElement
-};
+use stdweb::web::{document, html_element::CanvasElement, window, CanvasRenderingContext2d};
+use util::{get_range, qs};
 
-use stdweb::web::event::{
-  MouseMoveEvent,
-  ResizeEvent
-};
+use stdweb::web::event::{MouseMoveEvent, ResizeEvent};
 
 macro_rules! cursor {
-  ($el:expr, $x:expr, $y:expr) => {
-    $el.set_attribute("style", &format!(
-      "transform: translate3d({},{},0);", $x, $y
-    )).unwrap();
-  }
+    ($el:expr, $x:expr, $y:expr) => {
+        $el.set_attribute(
+            "style",
+            &format!("transform: translate3d({},{},0);", $x, $y),
+        )
+        .unwrap();
+    };
 }
 
 fn rect(context: &CanvasRenderingContext2d, x: f64, y: f64) {
-  let width = get_range(4.0, 40.0);
-  let height = get_range(4.0, 80.0);
-  let alpha = get_range(0.001, 0.1);
+    let width = get_range(4.0, 40.0);
+    let height = get_range(4.0, 80.0);
+    let alpha = get_range(0.001, 0.1);
 
-  let stroke_style_color = format!("rgba(0, 0, 0, {})", alpha);
+    let stroke_style_color = format!("rgba(0, 0, 0, {})", alpha);
 
-  context.begin_path();
-  context.rect(
-    f64::from(x as i32 - width as i32 / 2),
-    f64::from(y as i32 - height as i32 / 2),
-    width,
-    height
-  );
+    context.begin_path();
+    context.rect(
+        f64::from(x as i32 - width as i32 / 2),
+        f64::from(y as i32 - height as i32 / 2),
+        width,
+        height,
+    );
 
-  context.set_stroke_style_color(&stroke_style_color.to_string());
-  context.stroke();
+    context.set_stroke_style_color(&stroke_style_color.to_string());
+    context.stroke();
 }
 
-pub fn initialize () {
-  let canvas: CanvasElement = document().query_selector(".canvas").unwrap().unwrap().try_into().unwrap();
-  let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
+pub fn initialize() {
+    let canvas: CanvasElement = document()
+        .query_selector(".canvas")
+        .unwrap()
+        .unwrap()
+        .try_into()
+        .unwrap();
+    let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
 
-  canvas.set_width(canvas.offset_width() as u32);
-  canvas.set_height(canvas.offset_height() as u32);
-
-  let resize_event = move |_: ResizeEvent| {
     canvas.set_width(canvas.offset_width() as u32);
     canvas.set_height(canvas.offset_height() as u32);
-  };
 
-  let mut throttle_count = 0;
-  let throttle_every = 6;
+    let resize_event = move |_: ResizeEvent| {
+        canvas.set_width(canvas.offset_width() as u32);
+        canvas.set_height(canvas.offset_height() as u32);
+    };
 
-  let mouse_move_event = move |event: MouseMoveEvent| {
-    let x = f64::from(event.client_x());
-    let y = f64::from(event.client_y());
+    let mut throttle_count = 0;
+    let throttle_every = 6;
 
-    throttle_count += 1;
-    if throttle_count == throttle_every {
-      rect(&context, x, y);
-      throttle_count = 0;
-    }
+    let mouse_move_event = move |event: MouseMoveEvent| {
+        let x = f64::from(event.client_x());
+        let y = f64::from(event.client_y());
 
-    qs(".coord > div").set_text_content(&format!("_x: {}, _y: {}", x, y));
-    cursor!(qs(".x"), &format!("{}px", x), 0);
-    cursor!(qs(".y"), 0, &format!("{}px", y));
-    cursor!(qs(".cursor"), &format!("{}px", x), &format!("{}px", y));
-  };
+        throttle_count += 1;
+        if throttle_count == throttle_every {
+            rect(&context, x, y);
+            throttle_count = 0;
+        }
 
-  window().add_event_listener(resize_event);
-  window().add_event_listener(mouse_move_event);
+        qs(".coord > div").set_text_content(&format!("_x: {}, _y: {}", x, y));
+        cursor!(qs(".x"), &format!("{}px", x), 0);
+        cursor!(qs(".y"), 0, &format!("{}px", y));
+        cursor!(qs(".cursor"), &format!("{}px", x), &format!("{}px", y));
+    };
+
+    window().add_event_listener(resize_event);
+    window().add_event_listener(mouse_move_event);
 }
---
+++ src/trash.rs
@@ -0,0 +1,100 @@
+use stdweb::web::event::{
+    DragDropEvent, DragEndEvent, DragEnterEvent, DragEvent, DragLeaveEvent, DragOverEvent,
+    DragStartEvent,
+};
+
+use stdweb::traits::*;
+use stdweb::web::{document, CloneKind};
+use util::{nl, qs};
+
+use stdweb::unstable::TryInto;
+use stdweb::web::HtmlElement;
+
+pub fn initialize() {
+    let coord = qs(".coord");
+
+    /* drag and drop stuffs */
+    coord.add_event_listener(|event: DragOverEvent| {
+        event.prevent_default();
+    });
+
+    coord.add_event_listener(|event: DragEnterEvent| {
+        event.prevent_default();
+        let coord = qs(".coord");
+        coord.class_list().add("dragenter").unwrap();
+    });
+
+    coord.add_event_listener(|event: DragLeaveEvent| {
+        event.prevent_default();
+        let coord = qs(".coord");
+        coord.class_list().remove("dragenter").unwrap();
+    });
+
+    pub fn reset() -> () {
+        let coord = qs(".coord");
+        coord.class_list().remove("dragenter").unwrap();
+        coord.class_list().remove("trash").unwrap();
+        let drag = document().query_selector(".drag").unwrap();
+        if drag.is_some() {
+            js!( document.querySelector(".drag").classList.remove("drag"); )
+                .try_into()
+                .unwrap()
+        }
+    };
+
+    pub fn del() -> () {
+        let drag = qs(".drag");
+        drag.set_attribute("style", "display: none;").unwrap();
+        reset();
+    };
+
+    coord.add_event_listener(|event: DragDropEvent| {
+        event.prevent_default();
+        console!(log, "drop!");
+        js! {
+          var del = @{del};
+          var reset = @{reset};
+          if (window.confirm("U sure?")) {
+            del();
+            del.drop()
+          } else {
+            reset();
+            reset.drop()
+          }
+        }
+    });
+
+    for link in nl(".project.link, .cv.link") {
+        let el: HtmlElement = link.clone().try_into().unwrap();
+        let drag_event = |event: DragEvent| {
+            let cursor = qs(".cursor");
+            let x = f64::from(event.client_x());
+            let y = f64::from(event.client_y());
+            cursor
+                .set_attribute(
+                    "style",
+                    &format!("transform: translate3d({}px,{}px,0);", x, y),
+                )
+                .unwrap();
+        };
+        link.add_event_listener(enclose!( (el) move |_event: DragStartEvent| {
+          let coord = qs(".coord");
+          coord.class_list().add("trash").unwrap();
+          let cursor = qs(".cursor");
+          cursor.class_list().remove("zoom").unwrap();
+          let clone = el.clone_node(CloneKind::Deep).unwrap();
+          el.class_list().add("drag").unwrap();
+          cursor.append_child(&clone);
+          console!(log, coord);
+        }));
+        link.add_event_listener(drag_event);
+        link.add_event_listener(|_event: DragEndEvent| {
+            reset();
+            let cursor = qs(".cursor");
+            let project = qs(".cursor .project");
+            let projects = qs("._projects");
+            projects.remove_attribute("style");
+            cursor.remove_child(&project).unwrap();
+        });
+    }
+}
